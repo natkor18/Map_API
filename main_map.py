71511e273905478791f12ca469d30ecec9c4b1d3
natalia18 <natkor18@gmail.com>
2025-02-19 22:08:39 +0300

    add Session.get() + scale map

--- main_map.py
+++ main_map.py
@@ -6,6 +6,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from geocoder import geocoder, map_object
 
+
 class MapShow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -13,13 +14,36 @@ class MapShow(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        self.zoom = 5
         self.searchbtn.clicked.connect(self.run)
 
     def run(self):
-        adr, coords, spn = geocoder(self.address.text())
+        self.update_map()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp and self.zoom < 20:
+            self.zoom += 1
+            self.update_map()
+        if event.key() == Qt.Key.Key_PageDown and self.zoom > 1:
+            self.zoom -= 1
+            self.update_map()
+
+    def update_map(self):
+        self.adr, self.coords, self.spn = geocoder(self.address.text())
         self.file_map = 'map.png'
+
+        api_address = "https://static-maps.yandex.ru/v1?"
+        params = {
+            "ll": ','.join(self.coords.split()),
+            "z": f"{self.zoom}",
+            "pt": ','.join(self.coords.split()),
+            'apikey': YOU_APIKEY
+        }
+        sessia = requests.Session()
+        sessia_map = sessia.get(api_address, params=params)
+
         with open(self.file_map, "wb") as f:
-            f.write(map_object(coords, spn))
+            f.write(sessia_map.content)
 
         self.image = QPixmap(self.file_map)
         self.label.setPixmap(self.image)
